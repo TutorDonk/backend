482de02670cce64970349fc5a6966f211a900c0e
hafidzyami <18221074@std.stei.itb.ac.id>
2024-06-10 08:08:36 +0700

    add fake auth login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+import { LocalGuard } from './guards/local.guard';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { Request } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService : AuthService){}
+
+    @Post('login')
+    @UseGuards(LocalGuard)
+    login(@Req() req : Request){
+       return req.user
+    }
+
+    @Get('status')
+    @UseGuards(JwtAuthGuard)
+    status(@Req() req : Request){
+        console.log(req.user)
+        return req.user
+    }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRED_DURATION') },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
